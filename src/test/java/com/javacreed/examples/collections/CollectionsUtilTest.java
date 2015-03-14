13ea3c45c3f03131062d1c44162fa58359d0faf1
javacreed <albert@javacreed.com>
2015-03-14 14:49:46 +0100

    Updated the license documentation

--- src/test/java/com/javacreed/examples/collections/CollectionsUtilTest.java
+++ src/test/java/com/javacreed/examples/collections/CollectionsUtilTest.java
@@ -2,37 +2,22 @@
  * #%L
  * Sorting a CopyOnWriteArrayList
  * $Id:$
- * $HeadURL: https://java-creed-examples.googlecode.com/svn/collections/Sorting%20a%20CopyOnWriteArrayList/src/test/java/com/javacreed/examples/collections/CollectionsUtilTest.java $
+ * $HeadURL:$
  * %%
- * Copyright (C) 2012 - 2014 Java Creed
+ * Copyright (C) 2012 - 2015 Java Creed
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * #L%
- */
-/**
- * Copyright 2012-2014 Java Creed.
- *
- * Licensed under the Apache License, Version 2.0 (the "<em>License</em>");
- * you may not use this file except in compliance with the License. You may
- * obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
  */
 package com.javacreed.examples.collections;
 
